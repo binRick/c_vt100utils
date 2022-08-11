6f29e357c976445bf3b465103f65a461d89e7399
rick <rick@ricks-MacBook-Air.local>
2022-08-11 17:16:09 -0400

    add signal handler for resize

--- demos/tuibox.h
+++ demos/tuibox.h
@@ -1,16 +1,32 @@
-/*
- * tuibox.h: simple tui library
- */
-
-#ifndef TUIBOX_H
+#pragma once
 #define TUIBOX_H
-
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
+#define STDIN_ENABLED    true
+
+
+char *_strdup_escaped(char *tmp) {
+  char *ret = (char *)malloc(strlen(tmp) * 4 + 1);
+  char *dst = ret;
+
+  for ( ; *tmp; tmp++) {
+    if (*tmp >= ' ' && *tmp <= 126 && *tmp != '\\') {
+      *dst = *tmp;
+      ++dst;
+    } else {
+      dst += sprintf(dst, "\\x%02hhx", (unsigned char)*tmp);
+    }
+  }
+  *dst = 0;
+  return(ret);
+}
 
 /** BEGIN vec.h **/
 
@@ -173,6 +189,7 @@
 enum binding_mode_t {
   BINDING_MODE_MOUSE_SCROLL_UP,
   BINDING_MODE_MOUSE_SCROLL_DOWN,
+  BINDING_MODE_UNHANDLED_INPUT,
   BINDING_MODES_QTY,
 };
 struct binding_type_t {
@@ -184,6 +201,9 @@ struct binding_type_t *binding_types[] = {
     .handler = NULL,
   },
   [BINDING_MODE_MOUSE_SCROLL_DOWN] = &(struct binding_type_t){
+    .handler = NULL,
+  },
+  [BINDING_MODE_UNHANDLED_INPUT] = &(struct binding_type_t)  {
     .handler = NULL,
   },
   [BINDING_MODES_QTY] = NULL,
@@ -237,7 +257,7 @@ int vec_reserve_(char **data, int *length, int *capacity, int memsz, int n) {
 }
 
 
-int vec_reserve_po2_(char **data, int *length, int *capacity, int memsz, int n)                                                                               {
+int vec_reserve_po2_(char **data, int *length, int *capacity, int memsz, int n) {
   int n2 = 1;
 
   if (n == 0) {
@@ -271,7 +291,7 @@ int vec_compact_(char **data, int *length, int *capacity, int memsz) {
 
 
 int vec_insert_(char **data, int *length, int *capacity, int memsz,
-                int idx)                         {
+                int idx) {
   int err = vec_expand_(data, length, capacity, memsz);
 
   if (err) {
@@ -285,7 +305,7 @@ int vec_insert_(char **data, int *length, int *capacity, int memsz,
 
 
 void vec_splice_(char **data, int *length, int *capacity, int memsz,
-                 int start, int count)                                      {
+                 int start, int count) {
   (void)capacity;
   memmove(*data + start * memsz,
           *data + (start + count) * memsz,
@@ -294,7 +314,7 @@ void vec_splice_(char **data, int *length, int *capacity, int memsz,
 
 
 void vec_swapsplice_(char **data, int *length, int *capacity, int memsz,
-                     int start, int count)                                          {
+                     int start, int count) {
   (void)capacity;
   memmove(*data + start * memsz,
           *data + (*length - count) * memsz,
@@ -303,7 +323,7 @@ void vec_swapsplice_(char **data, int *length, int *capacity, int memsz,
 
 
 void vec_swap_(char **data, int *length, int *capacity, int memsz,
-               int idx1, int idx2)                                  {
+               int idx1, int idx2) {
   unsigned char *a, *b, tmp;
   int           count;
 
@@ -345,7 +365,10 @@ void vec_swap_(char **data, int *length, int *capacity, int memsz,
 #define UI_CENTER_Y    -1
 
 /* The argument isn't actually necessary here, but it helps with design consistency */
-#define ui_loop(u)       char buf[64]; int n; while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0)
+#define ui_loop(u)     \
+  signal(SIGWINCH, sig_handler);\
+  char buf[64]; int n; \
+  while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0)
 
 #define ui_update(u)     _ui_update(buf, n, u)
 
@@ -405,7 +428,7 @@ typedef struct ui_t {
                  scroll, canscroll,
                  id, force;
 } ui_t;
-
+static void tb_sig_handler(int sig);
 
 /* =========================== */
 
@@ -427,11 +450,17 @@ void ui_new(int s, ui_t *u){
   raw.c_lflag &= ~(ECHO | ICANON);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 
+
   vec_init(&(u->b));
   vec_init(&(u->e));
+  printf("\x1b[?1049h\x1b[0m\x1b[2J\x1b[?1003h\x1b[?1015h\x1b[?1006h\x1b[?25l");
+  printf("\x1b[?1004h");
+  printf("\x1b[?1002h");
+  printf("\x1b[?1000h");
+  fflush(stdout);
 
 
-  u->mouse = 0;
+  u->mouse = 1;
 
   u->screen    = s;
   u->scroll    = 1;
@@ -455,7 +484,11 @@ void ui_free(ui_t *u){
   char     *term;
 
   printf("\x1b[0m\x1b[2J\x1b[?1049l\x1b[?1003l\x1b[?1015l\x1b[?1006l\x1b[?25h");
+  printf("\x1b[?1004l");
+  fflush(stdout);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &(u->tio));
+  fflush(STDIN_FILENO);
+
 
   vec_foreach(&(u->b), val, i){
     free(val->cache);
@@ -491,7 +524,7 @@ int ui_add(int x, int y, int w, int h, int screen,
            char *watch, char initial,
            func draw, func onclick, func onhover,
            void *data1, void *data2,
-           ui_t *u)          {
+           ui_t *u) {
   char     *buf = malloc(MAXCACHESIZE);
 
   ui_box_t *b = malloc(sizeof(ui_box_t));
@@ -633,13 +666,19 @@ void ui_redraw(ui_t *u){
  *   variables buf and n remain
  *   opaque to the user.
  */
+static volatile bool stdin_flushed = false;
 
 
 void _ui_update(char *c, int n, ui_t *u){
+  if (false == stdin_flushed) {
+    fflush(stdin);
+    stdin_flushed = true;
+  }
   ui_box_t *tmp;
   ui_evt_t *evt;
   int      ind, x, y;
   char     cpy[n], *tok;
+  bool     is_handled = false;
 
   if (n >= 4
       && c[0] == '\x1b'
@@ -650,40 +689,85 @@ void _ui_update(char *c, int n, ui_t *u){
     if (strcmp(tok, "64") == 0) {
       if (binding_types[BINDING_MODE_MOUSE_SCROLL_UP] != NULL && binding_types[BINDING_MODE_MOUSE_SCROLL_UP]->handler != NULL) {
         binding_types[BINDING_MODE_MOUSE_SCROLL_UP]->handler((void *)NULL);
+        ui_draw(u);
+        is_handled = true;
       }
     }else if (strcmp(tok, "65") == 0) {
       if (binding_types[BINDING_MODE_MOUSE_SCROLL_DOWN] != NULL && binding_types[BINDING_MODE_MOUSE_SCROLL_DOWN]->handler != NULL) {
         binding_types[BINDING_MODE_MOUSE_SCROLL_DOWN]->handler((void *)NULL);
-      }
-    }
-    switch (tok[0]) {
-    case '0':
-      u->mouse = (strchr(c, 'm') == NULL);
-      if (u->mouse) {
-        COORDINATE_DECODE();
-        LOOP_AND_EXECUTE(tmp->onclick);
-      }
-      break;
-    case '3':
-      u->mouse = (strcmp(tok, "32") == 0);
-      COORDINATE_DECODE();
-      LOOP_AND_EXECUTE(tmp->onhover);
-      break;
-    case '6':
-      if (u->canscroll) {
-        u->scroll += (4 * (tok[1] == '4')) - 2;
-        printf("\x1b[0m\x1b[2J");
         ui_draw(u);
+        is_handled = true;
       }
-      break;
+    }else{
+      switch (tok[0]) {
+      case '0':
+        u->mouse = (strchr(c, 'm') == NULL);
+        if (u->mouse) {
+          COORDINATE_DECODE();
+          LOOP_AND_EXECUTE(tmp->onclick);
+          is_handled = true;
+        }
+        break;
+      case '3':
+        u->mouse = (strcmp(tok, "32") == 0);
+        COORDINATE_DECODE();
+        LOOP_AND_EXECUTE(tmp->onhover);
+        is_handled = true;
+        break;
+      case '6':
+        if (u->canscroll) {
+          u->scroll += (4 * (tok[1] == '4')) - 2;
+          printf("\x1b[0m\x1b[2J");
+          ui_draw(u);
+          is_handled = true;
+        }
+        break;
+      }
     }
   }
 
-  vec_foreach(&(u->e), evt, ind){
-    if (strncmp(c, evt->c, strlen(evt->c)) == 0) {
-      evt->f();
+  if (false == is_handled) {
+    vec_foreach(&(u->e), evt, ind){
+      if (strncmp(c, evt->c, strlen(evt->c)) == 0) {
+        evt->f();
+        is_handled = true;
+      }else{
+        char _cpy[n], *_tok, *__tok, *___tok;
+        strncpy(_cpy, _strdup_escaped(c), strlen(_strdup_escaped(c)));
+        _tok   = strtok(_cpy + 0, "\\x");
+        __tok  = strtok(_tok + 0, "1b");
+        ___tok = strtok(_tok + 0, "09");
+        if (strncmp(__tok, evt->c, strlen(evt->c)) == 0) {
+          evt->f();
+          is_handled = true;
+        }else if (strcmp(c, evt->c) == 0) {
+          evt->f();
+          is_handled = true;
+        }else if (strcmp(_strdup_escaped(c), _strdup_escaped(evt->c)) == 0) {
+          evt->f();
+          is_handled = true;
+        }else{
+        }
+      }
     }
   }
+  if (false == is_handled) {
+    if (binding_types[BINDING_MODE_UNHANDLED_INPUT] != NULL && binding_types[BINDING_MODE_UNHANDLED_INPUT]->handler != NULL) {
+      binding_types[BINDING_MODE_UNHANDLED_INPUT]->handler((void *)c);
+      is_handled = true;
+      ui_draw(u);
+    }
+
+    if (false == is_handled) {
+      fprintf(stderr,
+              "unhandled "
+              "\n\tc: '%s'||'%s' (%lu chars)"
+              "\n",
+              _strdup_escaped(c), c, strlen(c)
+              );
+    }
+  }
+  fflush(STDIN_FILENO);
 } // _ui_update
 
 
@@ -698,7 +782,7 @@ void _ui_text(ui_box_t *b, char *out){
 int ui_text(int x, int y, char *str,
             int screen,
             func click, func hover,
-            ui_t *u)          {
+            ui_t *u) {
   return(ui_add(
            x, y,
            strlen(str), 1,
@@ -713,4 +797,13 @@ int ui_text(int x, int y, char *str,
            ));
 }
 
-#endif
+static void tb_sig_handler(int sig){
+  if (SIGWINCH == sig) {
+    struct winsize winsz;
+
+    ioctl(0, TIOCGWINSZ, &winsz);
+    fprintf(stderr,"SIGWINCH raised, window size: %d rows / %d columns\n",winsz.ws_row, winsz.ws_col);
+    ui_redraw(u);
+  }
+
+}
